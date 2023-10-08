70b69fa172a14478c449ddf685ce6dd49697e943
Yasir Ali <yasirali478@gmail.com>
2023-10-08 22:45:49 +0500

    Add files via upload

--- Knn.py
+++ Knn.py
@@ -6,17 +6,23 @@ from sklearn.preprocessing import LabelEncoder
 from imblearn.combine import SMOTETomek
 import seaborn as sns
 import matplotlib.pyplot as plt
-df=pd.read_csv("DataTraining.csv")
-df2=pd.read_csv("DataTest.csv")
 import numpy as np
-x_train=df[["Humidity", "Light", "HumidityRatio"]]
-x_test=df2[["Humidity", "Light", "HumidityRatio"]]
-y_train=df["Occupancy"]
-y_test=df2["Occupancy"]
+df=pd.read_csv("cancer.csv")
+df.pop("Patient Id")
+le=LabelEncoder()
+df["Level"]=le.fit_transform(df["Level"])
+print(df.info())
+smk=SMOTETomek(random_state=42)
+y=df.pop("Level")
+df,y=smk.fit_resample(df,y)
+print(y.value_counts())
+x_train,x_test,y_train,y_test=train_test_split(df,y,test_size=0.2,random_state=1)
 p=x_train.shape[0]
 k=np.sqrt(p)
 Knn=KNeighborsClassifier(n_neighbors=int(k))
 kn1=Knn.fit(x_train,y_train)
 pred=Knn.predict(x_test)
 acc=accuracy_score(y_test,pred)
+train_acc=Knn.score(x_train,y_train)
 print(acc)
+print(train_acc)
